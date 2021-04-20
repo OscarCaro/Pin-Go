75d7ba62e904253df6802dc023af2e0b586a917f
Óscar Caro <oscaro@ucm.es>
2021-04-20 18:45:00 +0200

    Receive intent

--- app/src/main/java/com/pinandgo/MainActivity.kt
+++ app/src/main/java/com/pinandgo/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.pinandgo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView : TextView = findViewById(R.id.text)
+
+        when {
+            intent?.action == Intent.ACTION_SEND -> {
+                if ("text/plain" == intent.type) {
+                    //handleSendText(intent) // Handle text being sent
+                    textView.text = intent.getStringExtra(Intent.EXTRA_TEXT)
+                } else if (intent.type?.startsWith("image/") == true) {
+                    //handleSendImage(intent) // Handle single image being sent
+                }
+            }
+            intent?.action == Intent.ACTION_SEND_MULTIPLE && intent.type?.startsWith("image/") == true -> {
+                //handleSendMultipleImages(intent) // Handle multiple images being sent
+            }
+            else -> {
+                // Handle other intents, such as being started from the home screen
+            }
+        }
+
+
     }
 }

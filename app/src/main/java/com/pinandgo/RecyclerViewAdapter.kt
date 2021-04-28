1c8750bd6e3b876cfb44e450bd158bea5bc48990
Óscar Caro <oscaro@ucm.es>
2021-04-28 20:51:44 +0200

    RecyclerView with image and data

--- app/src/main/java/com/pinandgo/RecyclerViewAdapter.kt
+++ app/src/main/java/com/pinandgo/RecyclerViewAdapter.kt
@@ -5,14 +5,18 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class RecyclerViewAdapter(private val listSize : Int) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
     class ViewHolder (view : View) : RecyclerView.ViewHolder(view){
-        val linkTextView : TextView = view.findViewById(R.id.textLink)
-        val domainTextView : TextView = view.findViewById(R.id.textDomain)
+        val textTitle : TextView = view.findViewById(R.id.textTitle)
+        val textDesc : TextView = view.findViewById(R.id.textDesc)
+        val textDomain : TextView = view.findViewById(R.id.textDomain)
+        val imageView : ImageView = view.findViewById(R.id.image)
     }
 
     override fun onCreateViewHolder( parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,10 +26,17 @@ class RecyclerViewAdapter(private val listSize : Int) : RecyclerView.Adapter<Rec
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val pin = PinList.get(position, holder.itemView.context)
-        holder.linkTextView.text = pin.intentUrl
-        holder.domainTextView.text = pin.domain
+        holder.textTitle.text = pin.title
+        holder.textDesc.text = pin.description
+        holder.textDomain.text = pin.domain
 
-        holder.linkTextView.setOnClickListener{
+        Glide.with(holder.itemView.context)
+            .load(pin.imageUrl)
+            .fitCenter()
+            .into(holder.imageView)
+        //TODO: loading wheel placeholder and NoIntenet placeholder
+
+        holder.itemView.setOnClickListener{
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(pin.intentUrl)
             holder.itemView.context.startActivity(intent)
